bca6a7f9f9d32799f9b490a415275bc47a8c1817
majkoce <polo.havran@gmail.com>
2019-11-13 11:00:46 +0100

    On branch changeAlgorithm  Changes to be committed: 	modified:   proj1 	modified:   proj1.c 	modified:   zoznam.txt

--- proj1.c
+++ proj1.c
@@ -10,15 +10,15 @@
 
 int copyArgString(char *argv[], int *countsearchedString, char *searchedString);
 
-int searchForNumber(char *firstChar);
+int searchForNumber(char firstChar);
 
-int scanAndPrintContacts(int *correctInput, char *searchedString, int *countsearchedString);
+int scanAndPrintContacts(int correctInput, char *searchedString, int *countsearchedString);
 
-int searchForMatch(char *searchedString, int *countSearchedString, char entries[STRLENGTH][STRLENGTH], int *entryStringCount);
+int searchForMatch(char *searchedString, int *countSearchedString, char entries[STRLENGTH][STRLENGTH], int entryStringCount);
 
-int printContact (int *entryStringCount, char entries[STRLENGTH][STRLENGTH]);
+int printContact (int entryStringCount, char entries[STRLENGTH][STRLENGTH]);
 
-int main(int argc, char *argv[]){   
+int main(int argc, char *argv[]){
     char searchedString[STRLENGTH];
     int countsearchedString = 0;
     int countMatchedContacts = 0;
@@ -38,11 +38,11 @@ int main(int argc, char *argv[]){
         correctInput = copyArgString(argv, &countsearchedString, searchedString);
     }
     //call for main function
-    countMatchedContacts = scanAndPrintContacts(&correctInput, searchedString, &countsearchedString);
+    countMatchedContacts = scanAndPrintContacts(correctInput, searchedString, &countsearchedString);
     
     //bad states
     if(countMatchedContacts == 0 && correctInput != -1){
-        fprintf(stdout, "Not found\n");     
+        fprintf(stdout, "Not found\n");
     }
     if(correctInput == -1){
         fprintf(stderr, "Wrong Input\n");
@@ -58,7 +58,7 @@ int copyArgString(char *argv[], int *countsearchedString, char *searchedString){
     //returns -1 when argument is invalid
 
     for(int i = 0; argv[1][i] != '\0';i++){
-        int checkInput = searchForNumber(&argv[1][i]);
+        int checkInput = searchForNumber(argv[1][i]);
         // if argument is valid
         if (checkInput == 1 && *countsearchedString < STRLENGTH){
             searchedString[i] = argv[1][i];
@@ -72,17 +72,17 @@ int copyArgString(char *argv[], int *countsearchedString, char *searchedString){
     return 1;
 }
 
-int scanAndPrintContacts(int *correctInput, char *searchedString, int *countsearchedString){
+int scanAndPrintContacts(int correctInput, char *searchedString, int *countsearchedString){
     //scan contacts from stdin, search for matching contacts and print them
     //correctInput = states of correct input, if == 1, function work normaly, if == 0, function will print everything from stdin formated
     //searchedString = string which we want to compare
     //countsearchedString = count of chars in searchedString
     //returns number of matched and printed contacts
-    
+
     char entries[STRLENGTH][STRLENGTH];
     int checkForEOF = 0;
     int countMatchedContacts = 0;
-    while(checkForEOF != 1 && *correctInput != -1){
+    while(checkForEOF != 1 && correctInput != -1){
         int contactWasFound = 0;
         int entryStringCount = 0;
         //scan contact to entry
@@ -95,14 +95,14 @@ int scanAndPrintContacts(int *correctInput, char *searchedString, int *countsear
             }
         }
         //if there is correct input 
-        if (*correctInput != -1 && checkForEOF != 1){
+        if (correctInput != -1 && checkForEOF != 1){
             //search for match
-            if (*correctInput == 1){
-                contactWasFound = searchForMatch(searchedString, countsearchedString, entries, &entryStringCount);
+            if (correctInput == 1){
+                contactWasFound = searchForMatch(searchedString, countsearchedString, entries, entryStringCount);
             }
             //print matched contact
-            if (contactWasFound == 1 || *correctInput == 0){
-                printContact(&entryStringCount, entries);
+            if (contactWasFound == 1 || correctInput == 0){
+                printContact(entryStringCount, entries);
                 countMatchedContacts++;
             }
         }
@@ -110,12 +110,12 @@ int scanAndPrintContacts(int *correctInput, char *searchedString, int *countsear
     return countMatchedContacts;
 }
 
-int searchForNumber(char *firstChar){
+int searchForNumber(char firstChar){
     //search for number
     //firstChar = char that we want to check
     //returns 1 if its number
     //returns 0 if its char
-    if (*firstChar >= '0' && *firstChar <= '9'){
+    if (firstChar >= '0' && firstChar <= '9'){
         return 1; //number
     }
     else {
@@ -123,7 +123,7 @@ int searchForNumber(char *firstChar){
     }
 }
 
-int searchForMatch(char *searchedString, int *countSearchedString, char entries[STRLENGTH][STRLENGTH], int *entryStringCount){
+int searchForMatch(char *searchedString, int *countSearchedString, char entries[STRLENGTH][STRLENGTH], int entryStringCount){
     //check if contact is matching input parameter
     //searchedString = input parameter
     //countSearchedString = count of chars in searchedString
@@ -131,7 +131,6 @@ int searchForMatch(char *searchedString, int *countSearchedString, char entries[
     //entryStringCount = count of entries in entries
     // returns 1 when match was found
     // returns -1 when no match was found
-    
     const int countLettersByNumbers = 4;
     char lettersByNumbers[10][4] = {
         {'+'}, //0
@@ -147,44 +146,47 @@ int searchForMatch(char *searchedString, int *countSearchedString, char entries[
     };
     int searchedNumber = 0;
     int isNumber = 0;
-    for (int entryIndex = 0; entryIndex < *entryStringCount; entryIndex++){
+    for (int entryIndex = 0; entryIndex < entryStringCount; entryIndex++){
         int matchCount = 0;
-        int strLength = strlen(entries[entryIndex]);
-        for (int i = 0; i < strLength; i++){
-            isNumber = searchForNumber(&entries[entryIndex][i]);
-            //if current char is character
-            if (isNumber == 0){
-                //find match for every char represented by searched number
-                for (int j = 0; j < countLettersByNumbers; j++){
+        int entryLength = strlen(entries[entryIndex]);
+        if (entryStringCount <= entryLength){
+            for (int i = 0; i < entryLength; i++){
+                isNumber = searchForNumber(entries[entryIndex][i]);
+                //if current char is character
+                if (isNumber == 0){
                     char lowerChar = tolower(entries[entryIndex][i]);
                     //convert char number to int
                     searchedNumber = searchedString[matchCount] - '0';
-                    if (lettersByNumbers[searchedNumber][j] == lowerChar){
+                    //find match for every char represented by searched number
+                    for (int j = 0; j < countLettersByNumbers; j++){
+                        if (lettersByNumbers[searchedNumber][j] == lowerChar){
+                            matchCount++;
+                            break;
+                        }
+                    }
+                }
+                //if current char is number
+                if(isNumber == 1){
+                    if (searchedString[matchCount] == entries[entryIndex][i]){
                         matchCount++;
                     }
                 }
-            }
-            //if current char is number
-            if(isNumber == 1){
-                if (searchedString[matchCount] == entries[entryIndex][i]){
-                    matchCount++;
+                //if there is valid match
+                if (matchCount == *countSearchedString){
+                return 1;
                 }
-            }
-             //if there is valid match
-            if (matchCount == *countSearchedString){
-            return 1;
             }
         }
     }
     return -1;
 }
 
-int printContact (int *entryStringCount, char entries[STRLENGTH][STRLENGTH]){
+int printContact (int entryStringCount, char entries[STRLENGTH][STRLENGTH]){
     //prints contact
     //entries = array of entries from stdin
     //entryStringCount = count of entries in entries
     //returns 0 when its done
-    for(int i = 0; i < *entryStringCount; i++){
+    for(int i = 0; i < entryStringCount; i++){
         int entryLength = strlen(entries[i]);
         //delete end of line chars
         for(int j = 0; j <= entryLength; j++){
